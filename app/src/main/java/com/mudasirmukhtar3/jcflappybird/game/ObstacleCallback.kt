1253e54d1d20845f2ef3ad04fa44386d9c47d771
Mudasir Mukhtar <mudasir.chohan@hotmail.com>
2024-12-11 11:30:00 +0500

    Random

--- app/src/main/java/com/mudasirmukhtar3/jcflappybird/game/ObstacleCallback.kt
+++ app/src/main/java/com/mudasirmukhtar3/jcflappybird/game/ObstacleCallback.kt
@@ -5,4 +5,5 @@ import android.graphics.Rect
 interface ObstacleCallback {
     fun obstacleOffScreen(obstacle: Obstacle?)
     fun updatePosition(obstacle: Obstacle?, positions: ArrayList<Rect>)
+
 }
