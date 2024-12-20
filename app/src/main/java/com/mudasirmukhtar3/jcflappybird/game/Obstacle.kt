7a440fdce562384c4bcedaa1a1afcde426e0bab5
Mudasir Mukhtar <mudasir.chohan@hotmail.com>
2024-12-20 11:30:00 +0500

    Random

--- app/src/main/java/com/mudasirmukhtar3/jcflappybird/game/Obstacle.kt
+++ app/src/main/java/com/mudasirmukhtar3/jcflappybird/game/Obstacle.kt
@@ -27,6 +27,7 @@ class Obstacle(
         resources.getDimension(R.dimen.obstacle_min_position).toInt()
     private val image: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.pipes)
     private val callback: ObstacleCallback = callback
+
     init {
         val random = Random(System.currentTimeMillis())
         height =
